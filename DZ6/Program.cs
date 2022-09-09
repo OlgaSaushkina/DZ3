cd231c8b20cc2adaedb893409c899d2d47aac6fb
«OlgaSaushkina» <soljuwka@gmail.com>
2022-09-09 20:27:11 +0700

    add DZ7

--- DZ6/Program.cs
+++ DZ6/Program.cs
@@ -2,36 +2,29 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
-Console.Clear();
+/* Console.Clear();
 Console.WriteLine("Введите число (указывающее сколько чисел Вы хотите ввести): ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine();
 int count=0;
 Console.WriteLine("Введите числa: ");
-
-int[] arr = new int[a];
-
-    for (int i = 0; i < a; ++i)
-    
-    {   
-        int n= int.Parse(Console.ReadLine()); 
-       
-        if (n> 0)
-        {
-            count=count+1;
-        }
-              
+for (int i = 0; i < a; ++i)
+{   
+    int n= int.Parse(Console.ReadLine()); 
+    if (n> 0)
+    {
+        count=count+1;
     }
+}
 Console.WriteLine();
-Console.WriteLine("Количество чисел больших 0  равно   " + count); 
-
+Console.WriteLine("Количество чисел больших 0  равно   " + count);  */
 
 ////////////////////////////////////////////////
 
 //Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-/* Console.WriteLine("Для того чтобы найти точку пересечения двух прямых, введите значения: ");
+Console.WriteLine("Для того чтобы найти точку пересечения двух прямых, введите значения: ");
 Console.WriteLine();
 Console.WriteLine("Введите значения: ");
 Console.Write("b1 = ");
@@ -56,4 +49,4 @@ double y = k1 * x + b1;
 
 Console.WriteLine("Точка пересечения двух прямых -> " + (x, y));
 }
-Console.WriteLine(); */
+Console.WriteLine();
---
+++ DZ7/Program.cs
@@ -0,0 +1,175 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+Console.Clear();
+double[,] GetArray(int m, int n) 
+{
+  double[,] result = new double[m, n];
+  for (int i = 0; i < m; ++i) 
+  {
+    for (int j = 0; j < n; ++j) 
+    {
+      result[i, j] = new Random().NextDouble()*30-10;
+    }
+  }
+  return result;
+}
+void PrintArray(double[,] array) 
+{
+  for (int i = 0; i < array.GetLength(0); ++i) 
+  {
+    for (int j = 0; j < array.GetLength(1); ++j) 
+    {
+      Console.Write(array[i, j] + "  ");
+    }
+    Console.WriteLine();
+  }
+}
+Console.WriteLine("Задайте размер массива: ");
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns);
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); ++i) 
+{
+    for (int j = 0; j < array.GetLength(1); ++j)
+    {
+        array[i,j]=Math.Round(array[i,j], 1);               
+    }
+}
+PrintArray(array);
+Console.WriteLine();
+
+/////////////////////////////////////////
+
+
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+/* Console.Clear();
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            result[i, j] = new Random().Next(0, 21);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); ++j)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Задайте размер массива: ");
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int flag=0;
+for (int i = 0; i < array.GetLength(0); ++i)
+{
+    for (int j = 0; j < array.GetLength(1); ++j)
+    {      
+        if (num ==array[i,j]){
+            flag =1;
+            break;
+        }                           
+    }  
+}
+if(flag==1)
+{
+   Console.WriteLine(num + " -> такое число есть в массиве."); 
+} 
+if(flag==0)
+{
+   Console.WriteLine(num + " -> такого числа нет в массиве.");    
+}   
+    
+Console.WriteLine(); */
+
+
+
+///////////////////////////////////////////////////////////
+//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+/* Console.Clear();
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); ++j)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Задайте размер массива: ");
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое элементов по столбцам:");
+for (int j = 0; j < array.GetLength(1); ++j)
+{
+    double sum=0;
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {      
+        sum=sum+array[i,j];            
+    }   
+double sredarif=sum/(array.GetLength(0));  
+Console.Write( + sredarif+ "  ");
+}  
+Console.WriteLine();  */
+
